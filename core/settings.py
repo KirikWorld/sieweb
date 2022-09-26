694940b42c0ac2424175866bfc3c7438fafbaa5a
KirikWorld <kuleshovk455@gmail.com>
2022-09-26 18:25:20 +0300

    fixed staticfiles

--- core/settings.py
+++ core/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = Path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    Path.join(BASE_DIR, 'static'),
+    Path.joinpath(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
